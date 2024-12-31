8d9ceb721be2bc351ea91d6d3be14ec3d40f89e8
Juan Andrés <juanandres@gtrz.local>
2024-12-31 15:48:44 -0500

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navigation from "./components/navigation/Navigation";
 import Home from "./components/home/Home";
 import UserProfileSettings from "./components/storysection/StorySection";
 
+ const uri = "mongodb+srv://oneDotadmin:<db_password>@onedot-dev.k2vqj.mongodb.net/?retryWrites=true&w=majority&appName=oneDot-dev"
+
 export default function App() {
   return (
     <div className="App">
